a533e955b0cdebde5c44a11b0d075d83f3494e0c
Tobias Tengler <45513122+tobias-tengler@users.noreply.github.com>
2021-05-16 12:03:54 +0200

    Add basic example tabs

---
+++ website2/src/components/ExampleTabs.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Tabs from "@theme/Tabs";
+import TabItem from "@theme/TabItem";
+
+export function ExampleTabs({ children }) {
+  return (
+    <Tabs
+      defaultValue="annotation-based"
+      groupId="coding-approach"
+      values={[
+        { label: "Annotation-based", value: "annotation-based" },
+        { label: "Code-first", value: "code-first" },
+        { label: "Schema-first", value: "schema-first" },
+      ]}
+    >
+      {children}
+    </Tabs>
+  );
+}
+
+function Annotation({ children }) {
+  return <TabItem value="annotation-based">{children}</TabItem>;
+}
+
+function Code({ children }) {
+  return <TabItem value="code-first">{children}</TabItem>;
+}
+
+function Schema({ children }) {
+  return <TabItem value="schema-first">{children}</TabItem>;
+}
+
+ExampleTabs.Annotation = Annotation;
+ExampleTabs.Code = Code;
+ExampleTabs.Schema = Schema;
